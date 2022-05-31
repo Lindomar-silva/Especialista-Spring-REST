989068d9c6130a69c8ed39dc04e09b8aecfbffa4
Lindomar-silva <lindomar.s.silva@gmail.com>
2022-05-30 21:17:44 -0300

    Meu código-fonte

---
+++ 2.16. Ambiguidade de beans e injeção de lista de beans/algafood-api/src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -0,0 +1,16 @@
+package com.algaworks.algafood.di.notificacao;
+
+import org.springframework.stereotype.Component;
+
+import com.algaworks.algafood.di.modelo.Cliente;
+
+@Component
+public class NotificadorEmail implements Notificador {
+
+	@Override
+	public void notificar(Cliente cliente, String msg) {
+
+		System.out.printf("Notificação %s através do e-mail %s: %s\n", 
+				cliente.getNome(), cliente.getEmail(), msg);
+	}
+}
