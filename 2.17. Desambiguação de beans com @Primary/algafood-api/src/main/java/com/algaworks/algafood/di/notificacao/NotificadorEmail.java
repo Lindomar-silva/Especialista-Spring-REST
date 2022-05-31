c51d2ba7e037a4cd5b6f2033ee1fa8699d0a2a34
Lindomar-silva <lindomar.s.silva@gmail.com>
2022-05-30 21:25:59 -0300

    Meu código-fonte

---
+++ 2.17. Desambiguação de beans com @Primary/algafood-api/src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -0,0 +1,17 @@
+package com.algaworks.algafood.di.notificacao;
+
+import org.springframework.stereotype.Component;
+
+import com.algaworks.algafood.di.modelo.Cliente;
+
+//@Primary
+@Component
+public class NotificadorEmail implements Notificador {
+
+	@Override
+	public void notificar(Cliente cliente, String msg) {
+
+		System.out.printf("Notificação %s através do e-mail %s: %s\n", 
+				cliente.getNome(), cliente.getEmail(), msg);
+	}
+}
---
+++ 2.17. Desambiguação de beans com @Primary/algafood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -0,0 +1,22 @@
+package com.algaworks.algafood.di.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.algaworks.algafood.di.modelo.Cliente;
+import com.algaworks.algafood.di.notificacao.Notificador;
+
+@Component
+public class AtivacaoClienteService {
+
+	@Autowired
+	private Notificador notificador;
+
+	public void ativar(Cliente cliente) {
+		cliente.Ativar();
+
+		notificador.notificar(cliente, "Cadastro ativo!");
+
+	}
+
+}
