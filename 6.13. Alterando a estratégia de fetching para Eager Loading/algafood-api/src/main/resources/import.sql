22960cd6bb8a62e6891783569b497880eef16a96
Lindomar-silva <lindomar.s.silva@gmail.com>
2022-07-03 14:46:56 -0300

    Meu código-fonte

---
+++ 6.13. Alterando a estratégia de fetching para Eager Loading/algafood-api/src/main/resources/import.sql
@@ -0,0 +1,39 @@
+INSERT INTO cozinha (nome) VALUES 
+('Tailandesa'), ('Indiana'), ('Brasileira'), ('Argentina');
+
+INSERT INTO estado (id, nome) VALUES 
+(1, 'São Paulo'), (2, 'Minas Gerais'), (3, 'Paraná');
+
+INSERT cidade (nome, estado_id) VALUES 
+('Americana', 1), ('Santa Barabara d''Oeste', 1), ('Urberlandia', 2), ('Belo Horizonte', 2), ('Londrina', 3), ('Ivaiporâ', 3);
+
+INSERT INTO restaurante (nome, taxa_frete, cozinha_id, endereco_cidade_id, endereco_cep, endereco_logradouro, endereco_numero, endereco_bairro, data_cadastro, data_atualizacao) VALUES
+('Thai Gourmet', 10, 1, 1, '38400-999', 'Rua João Pinheiro', '1000', 'Centro', UTC_TIMESTAMP, UTC_TIMESTAMP),
+('Thai Delivery', 9.50, 1, 2, '13588-000', 'Rua 13 de Maio', '200', 'Jardim 2', UTC_TIMESTAMP, UTC_TIMESTAMP),
+('Tuk Tuk Comida Indiana', 15.00, 2, 3, '13588-000', 'Rua 13 de Maio', '200', 'Jardim 2', UTC_TIMESTAMP, UTC_TIMESTAMP),
+('Panela de Ferro', 0, 3, 4, '13588-000', 'Rua 13 de Maio', '200', 'Jardim 2', UTC_TIMESTAMP, UTC_TIMESTAMP),
+('Fogão a Lemha', 0, 3, 2, '13588-000', 'Rua 13 de Maio', '200', 'Jardim 2', UTC_TIMESTAMP, UTC_TIMESTAMP),
+('Java Steakhouse', 12, 3, NULL, NULL, NULL, NULL, NULL, UTC_TIMESTAMP, UTC_TIMESTAMP),
+('Lanchonete do Tio Sam', 11, 4, NULL, NULL, NULL, NULL, NULL, UTC_TIMESTAMP, UTC_TIMESTAMP),
+('Bar da Maria', 6, 4, NULL, NULL, NULL, NULL, NULL, UTC_TIMESTAMP, UTC_TIMESTAMP);
+
+INSERT INTO produto (nome, descricao, preco, ativo, restaurante_id) VALUES 
+('Porco com molho agridoce', 'Deliciosa carne suína ao molho especial', 78.90, 1, 1),
+('Camarão tailandês', '16 camarões grandes ao molho picante', 110, 1, 1),
+('Salada picante com carne grelhada', 'Salada de folhas com cortes finos de carne bovina grelhada e nosso molho especial de pimenta vermelha', 87.20, 1, 2),
+('Garlic Naan', 'Pão tradicional indiano com cobertura de alho', 21, 1, 3),
+('Murg Curry', 'Cubos de frango preparados com molho curry e especiarias', 43, 1, 3),
+('Bife Ancho', 'Corte macio e suculento, com dois dedos de espessura, retirado da parte dianteira do contrafilé', 79, 1, 4),
+('T-Bone', 'Corte muito saboroso, com um osso em formato de T, sendo de um lado o contrafilé e do outro o filé mignon', 89, 1, 4),
+('Sanduíche X-Tudo', 'Sandubão com muito queijo, hamburger bovino, bacon, ovo, salada e maionese', 19, 1, 5),
+('Espetinho de Cupim', 'Acompanha farinha, mandioca e vinagrete', 8, 1, 6);
+
+INSERT INTO forma_pagamento (descricao) VALUES
+('Cartão de crédito'), ('Cartão de débito'), ('Dinheiro');
+
+INSERT INTO permissao (nome, descricao) VALUES
+('Admin', 'Permissão full para administrador do sistema'), ('Consultar produto', 'Permite somente consulta de produtos');
+
+INSERT INTO restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) VALUES 
+(1, 1), (1, 2), (1, 3), (2, 3), (3, 2), (3, 3), (4, 1), (4, 2), (5, 1), (5, 2), (6, 3);
+
---
+++ 6.7. Desafio: mapeando relacionamento muitos-para-um/algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -0,0 +1,122 @@
+package com.algaworks.algafood.api.controller;
+
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.repository.RestauranteRepository;
+import com.algaworks.algafood.domain.service.CadastroRestauranteService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@RestController
+@RequestMapping("/restaurantes")
+public class RestauranteController {
+
+	@Autowired
+	private RestauranteRepository restauranteRepo;
+
+	@Autowired
+	private CadastroRestauranteService restauranteService;
+
+	@GetMapping
+	public List<Restaurante> listar() {
+		return restauranteRepo.findAll();
+	}
+
+	@GetMapping("/{restauranteId}")
+	public ResponseEntity<Restaurante> buscar(@PathVariable Long restauranteId) {
+		Optional<Restaurante> restaurante = restauranteRepo.findById(restauranteId);
+
+		if (restaurante.isPresent()) {
+			return ResponseEntity.ok(restaurante.get());
+		}
+
+		return ResponseEntity.notFound().build();
+	}
+
+	@PostMapping
+	public ResponseEntity<?> adicionar(@RequestBody Restaurante restaurante) {
+		try {
+
+			restaurante = restauranteService.salvar(restaurante);
+			return ResponseEntity.status(HttpStatus.CREATED).body(restaurante);
+
+		} catch (EntidadeNaoEncontradaException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
+
+	@PutMapping("/{restauranteId}")
+	public ResponseEntity<?> atualizar(@PathVariable Long restauranteId, @RequestBody Restaurante restaurante) {
+		try {
+
+			Restaurante restauranteAtual = restauranteRepo.findById(restauranteId).orElse(null);
+
+			if (restauranteAtual != null) {
+//				BeanUtils.copyProperties(restaurante, restauranteAtual, "id", "formaPagamentos", "endereco", "dataCadastro");
+//				restauranteAtual = restauranteService.salvar(restauranteAtual);
+//				return ResponseEntity.ok(restauranteAtual);
+				
+				restaurante.setId(restauranteId);
+				restaurante.setDataCadastro(restauranteAtual.getDataCadastro());
+				restaurante.setFormaPagamentos(restauranteAtual.getFormaPagamentos());
+				restaurante.setEndereco(restauranteAtual.getEndereco());
+				restaurante = restauranteService.salvar(restaurante);
+//
+				return ResponseEntity.ok(restaurante);
+			}
+
+			return ResponseEntity.notFound().build();
+			
+		} catch (EntidadeNaoEncontradaException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
+	
+	@PatchMapping("/{restaranteId}")
+	public ResponseEntity<?> atualizarParcial(@PathVariable Long restaranteId, 
+			@RequestBody Map<String, Object> campos) {
+		Restaurante restauranteAtual = restauranteRepo.findById(restaranteId).orElse(null);
+		
+		if (restauranteAtual == null) {
+			return ResponseEntity.notFound().build();
+		}
+		merge(campos, restauranteAtual);
+		
+		return atualizar(restaranteId, restauranteAtual);
+	}
+
+	private void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino) {
+		ObjectMapper objectMapper = new ObjectMapper();
+		Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
+
+		System.out.println(restauranteOrigem);
+
+		dadosOrigem.forEach((nomeProp, valorProp) -> {
+			Field field = ReflectionUtils.findField(Restaurante.class, nomeProp);
+			field.setAccessible(true);
+
+			Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
+
+			System.out.println(nomeProp + ": " + valorProp + " NovoValor: " + novoValor);
+
+			ReflectionUtils.setField(field, restauranteDestino, novoValor);
+		});
+	}
+}
