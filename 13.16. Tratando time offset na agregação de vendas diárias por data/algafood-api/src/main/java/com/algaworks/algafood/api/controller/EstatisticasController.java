1c5f01cb1c627d14755d0345c50475515c970192
Lindomar-silva <lindomar.s.silva@gmail.com>
2022-09-26 17:15:29 -0300

    Meu código-fonte

---
+++ 13.16. Tratando time offset na agregação de vendas diárias por data/algafood-api/src/main/java/com/algaworks/algafood/api/controller/EstatisticasController.java
@@ -0,0 +1,27 @@
+package com.algaworks.algafood.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.algafood.domain.filter.VendaDiariaFilter;
+import com.algaworks.algafood.domain.model.dto.VendaDiaria;
+import com.algaworks.algafood.domain.service.VendaQueryService;
+
+@RestController
+@RequestMapping("/estatisticas")
+public class EstatisticasController {
+
+	@Autowired
+	private VendaQueryService queryService;
+
+	@GetMapping("/vendas-diarias")
+	public List<VendaDiaria> consultareVendasDiarias(VendaDiariaFilter filtro, 
+			@RequestParam(required = false, defaultValue = "+00:00") String timeOffset) {
+		return queryService.consultarVendasDiarias(filtro, timeOffset);
+	}
+}
---
+++ 13.19. Estruturando endpoint e serviço de emissão de relatório em PDF/algafood-api/src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
@@ -0,0 +1,25 @@
+package com.algaworks.algafood.api.exceptionhandler;
+
+import lombok.Getter;
+
+@Getter
+public enum ProblemType {
+
+	DADOS_INVALIDOS("/dados-invalidos", "Dados inválidos"),
+	ERRO_DE_SISTEMA("/erro-de-sistema", "Erro de Sistema"),
+	PARAMETRO_INVALIDO("/paramentro-invalido", "Parâmetro inválido"),
+	REQUISICAO_INVALIDA("/requisicao-invalida", "Requisição inválida"),
+	ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso"),
+	NEGOCIO_EXCEPTION("/erro-negocio", "Violação de regra de negócio"),
+	RECURSO_NAO_ENCONTRADO("/recurso-nao-encontrado", "Recurso não encontrado"),
+	METODO_NAO_PERMTIDO("/metodo-nao-permitido", "Método não permitido");
+	
+	private String title;
+	private String uri;
+
+	private ProblemType(String path, String title) {
+		this.title = title;
+		this.uri = "https://algafood.com.br" + path;
+	}
+
+}
